76b14484b68e4e00c713625015230cfbd5bbbad1
Pedro A. S. O. Neto <pasoneto@gmail.com>
2022-02-15 12:38:24 +0200

    Add files via upload

---
+++ functions.js
@@ -0,0 +1,54 @@
+function dropHandler(ev) {
+  ev.preventDefault();
+  if (ev.dataTransfer.items[0].kind === 'file') {
+     var file = ev.dataTransfer.items[0].getAsFile();
+     document.getElementById("p1").innerHTML = 'You selected the song:<br>"' + file.name.substring(2, file.name.length - 4)+'"';
+	}
+  return(file)
+}
+
+function analyzer(){
+  var file = document.getElementById("p1").innerHTML;
+  if(file !== ''){
+	var tasks = ['Analyzing "'+ file.substring(3, file.length - 4)+'"', "Analyzing artist data...", "Computing hit score"]
+	move(20, tasks[0])
+	setTimeout(() => { move(30, tasks[1]); }, 2000);
+	setTimeout(() => { move(50, tasks[2]); }, 5000);
+	setTimeout(() => {
+		document.getElementById("myProgress").style.backgroundColor = "white";
+		document.getElementById("p1").innerHTML = "Hit score is:" + 100;
+	}, 10000)
+  } else{
+	  alert("Please, choose a file.")
+  }
+}
+
+function move(time, text) {
+  var i = 0;
+  document.getElementById("p1").innerHTML = text;
+  if (i == 0) {
+    i = 1;
+    var elem = document.getElementById("myBar");
+    var elem2 = document.getElementById("myProgress");
+    elem2.style.backgroundColor = "#ddd"
+    var width = 1;
+    var id = setInterval(frame, time);
+    function frame() {
+      if (width >= 100) {
+        clearInterval(id);
+        i = 0;
+      } else {
+        width++;
+        elem.style.width = width + "%";
+        if(width == 100){
+          elem.style.width = "0%";
+        }
+      }
+    }
+  }
+}
+
+function dragOverHandler(ev) {
+  ev.preventDefault();
+  console.log(ev.dataTransfer.items)
+}
