4728eb3e9473dd2dd02fd2facee94d4ae6d0c07f
Pedro Neto <pdealcan@jyu-mac-20210915.local>
2022-02-16 11:56:31 +0200

    hps

--- functions.js
+++ functions.js
@@ -2,21 +2,35 @@ function dropHandler(ev) {
   ev.preventDefault();
   if (ev.dataTransfer.items[0].kind === 'file') {
      var file = ev.dataTransfer.items[0].getAsFile();
-     document.getElementById("p1").innerHTML = 'You selected the song:<br>"' + file.name.substring(2, file.name.length - 4)+'"';
+     document.getElementById("p1").innerHTML = 'You selected the file: "' + file.name + '"';
 	}
   return(file)
 }
 
+function hitPotential(file){
+  console.log(file)
+  if(file == "one.wav"){
+    return(98)
+  } else if(file == "two.wav"){
+    return(85)
+  } else if(file == "three.wav"){
+    return(79)
+  } else{
+    return(Math.floor(Math.random() * 100))
+  }
+}
+
 function analyzer(){
   var file = document.getElementById("p1").innerHTML;
+  var file = file.substring(24, file.length-1)
   if(file !== ''){
-	var tasks = ['Analyzing'+ file.substring(0, file.length - 4), "Analyzing artist data...", "Computing hit score"]
+	var tasks = ['Analyzing "'+ file + '"', "Analyzing artist data...", "Computing hit score"]
 	move(20, tasks[0])
 	setTimeout(() => { move(30, tasks[1]); }, 2000);
 	setTimeout(() => { move(50, tasks[2]); }, 5000);
 	setTimeout(() => {
 		document.getElementById("myProgress").style.backgroundColor = 'rgb(26, 26, 26)';
-    document.getElementById("p1").innerHTML = "Hit Potential score is: <strong><span style='color:rgb(255, 255, 51)'>" + Math.floor(Math.random() * 100) + "</span></strong>";
+    document.getElementById("p1").innerHTML = "Hit Potential score is: <strong><span style='color:rgb(255, 255, 51)'>" + hitPotential(file) + "</span></strong>";
 	}, 10000)
   } else{
 	  alert("Please, choose a file.")
